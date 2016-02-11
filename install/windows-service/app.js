29720f84a51e87b9c8bd3aa3bd8db9ba0ebe2b5e
Vladimir Tisma <tishma@Vladimirs-MacBook-Pro.local>
2016-02-11 17:10:24 +0100

    added service description

--- install/windows-service/app.js
+++ install/windows-service/app.js
@@ -5,6 +5,7 @@ var Service = require('node-windows').Service;
 // Create a new service object
 var svc = new Service({
     name: 'TestFairy Connect',
+    description: 'Integrate TestFairy with TFS WorkItem Collection.',
     script: process.cwd() + '/install/windows-service/app.js'
 });
 
