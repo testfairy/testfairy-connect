1995ef9849dc98224e8bc329acaa5139e909cdc8
Vladimir Tisma <tishma@Vladimirs-MacBook-Pro.local>
2016-02-11 16:31:15 +0100

    attempting to automate node-windows installation - only on windows

--- install/postinstall.js
+++ install/postinstall.js
@@ -2,8 +2,15 @@
 
 switch (process.platform) {
 case 'win32':
-    require(__dirname + '/windows-service/app.js');
-    process.exit(0);
+    process.exec("npm install -g node-windows & npm link node-windows", function (error, stdout, stderr) {
+        if (error) {
+            console.error(error);
+            process.exit(-1);
+        } else {
+            require(__dirname + '/windows-service/app.js');
+            process.exit(0);
+        }
+    });
     break;
 default:
     console.warn('Platform not supported: ' + process.platform + '. Please run TestFairy Connect manually.');
