86acb07951b56c6783540195afe099c99563a981
Vladimir Tisma <tishma@Vladimirs-MacBook-Pro.local>
2016-02-16 13:04:45 +0100

    fixed escaping double quotes

--- install/windows-service/event-logger.js
+++ install/windows-service/event-logger.js
@@ -7,14 +7,18 @@
  * @returns {{log: info, info: info, error: error, warn: warn}}
  */
 var logger = function (wrapped) {
+    function escapeArguments(args) {
+        return JSON.stringify(args).replace(/\"/g, '\\"').replace(/\\+/g, '\\').replace(/(?:\\[rnt])+/g, ' ');
+    }
+
     var info = function () {
-            wrapped.info(JSON.stringify(arguments).replace(/"/g, '\\"').replace(/(?:\\[rnt])+/g, ' '));
+            wrapped.info(escapeArguments(arguments));
         },
         error = function () {
-            wrapped.error(JSON.stringify(arguments).replace(/"/g, '\\"').replace(/(?:\\[rnt])+/g, ' '));
+            wrapped.error(escapeArguments(arguments));
         },
         warn = function () {
-            wrapped.warn(JSON.stringify(arguments).replace(/"/g, '\\"').replace(/(?:\\[rnt])+/g, ' '));
+            wrapped.warn(escapeArguments(arguments));
         };
     return {
         'log': info,
